5ed176127f34aed0caa1f46a1e1b0546a551d033
MonicaPiemontesi <105297815+MonicaPiemontesi@users.noreply.github.com>
2023-06-20 14:16:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+window.onload=function(){
+  var outerGifs = document.querySelectorAll('.jsFollow'),
+      innerGifs = document.querySelectorAll('.jsFollow > .innerGif'),
+      innerGifImages = document.querySelectorAll('.jsFollow > .innerGif > .image');
+  window.onmousemove = rotateElems;
+  function rotateElems(e) {
+    var mouseX = e.pageX,
+        mouseY = e.pageY;
+    for(var i=0; i < innerGifs.length; i++) {
+      var outerGif = outerGifs[i],
+          innerGif = innerGifs[i],
+          innerGifImage = innerGifImages[i],
+          centerX = outerGif.offsetLeft + (outerGif.clientWidth / 2),
+          centerY = outerGif.offsetTop + (outerGif.clientHeight / 2),
+          degree = Math.atan2(mouseX - centerX, - (mouseY - centerY) )*(180/Math.PI);    
+      innerGif.style.webkitTransform = "rotate(" + degree + "deg)";
+      innerGif.style.transform = "rotate(" + degree + "deg)";
+      degree = -degree;
+      innerGifImage.style.webkitTransform = "rotate(" + degree + "deg)";
+      innerGifImage.style.transform = "rotate(" + degree + "deg)";
+    }
